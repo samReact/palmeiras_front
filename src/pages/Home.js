01cbc0588c62e422435aae2e51611a3d2afba2ea
samReact <sam.react001@gmail.com>
2021-11-12 11:21:49 +0100

    :construction: home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,96 @@
-import { Grid, TextField } from '@mui/material'
+import { Button, Grid, TextField } from '@mui/material'
 import { useContext } from 'react'
 import { Context } from '../App'
+import Tooltip from '@mui/material/Tooltip'
 
 export default function Home() {
   const [state, dispatch] = useContext(Context)
-  const { carsA, carsB, staff } = state
+  const { carsA, carsB } = state
   const onChange = (e) => {
     dispatch({ type: 'handleStaff', payload: e.target.value })
   }
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <div>Staff: {staff}</div>
-        </Grid>
-        <Grid item xs={6}>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        style={{ marginTop: 100 }}
+      >
+        <Grid item xs={6} style={{ textAlign: 'center' }}>
+          <div>A model</div>
           {carsA}
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={6} style={{ textAlign: 'center' }}>
+          <div>B model</div>
           {carsB}
         </Grid>
       </Grid>
-      <TextField
-        id="outlined-number"
-        label="Available staff"
-        type="number"
-        onChange={onChange}
-      />
+
+      <Grid
+        style={{ marginTop: '6rem' }}
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="row"
+        spacing={{ xs: 1, sm: 3, md: 4 }}
+      >
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">Tires</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">Engines</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">Doors</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">Chassis</Button>
+          </Tooltip>
+        </Grid>
+      </Grid>
+      <Grid
+        style={{ marginTop: '10px' }}
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="row"
+        spacing={{ xs: 1, sm: 3, md: 4 }}
+      >
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">CarsA</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Delete">
+            <Button variant="contained">CarsB</Button>
+          </Tooltip>
+        </Grid>
+      </Grid>
+      <Grid
+        style={{ marginTop: '3rem' }}
+        container
+        justifyContent="center"
+        direction="row"
+      >
+        <Grid item>
+          <TextField
+            id="outlined-number"
+            label="Enter available staff"
+            type="number"
+            onChange={onChange}
+          />
+        </Grid>
+      </Grid>
     </>
   )
 }
