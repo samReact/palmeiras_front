d2c37928fa12fb3bdc980eaf9f654ca6401bd619
samReact <sam.react001@gmail.com>
2021-11-12 16:53:54 +0100

    :construction: home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import { Container } from '@mui/material'
 import { createContext, useReducer } from 'react'
+
 import Home from './pages/Home'
 import NavBar from './parts/NavBar'
 import './styles/App.css'
 
-const initialState = { staff: 0, carsA: 0, carsB: 0 }
+const initialState = {
+  staff: 0,
+  carsA: 0,
+  carsB: 0,
+  tires: 0,
+  engines: 0,
+  doors: 0,
+  chassis: 0,
+}
 
 function reducer(state, action) {
   const { type, payload } = action
   switch (type) {
     case 'handleStaff':
       return { ...state, staff: payload }
+    case 'handleProduction':
+      return {
+        ...state,
+        [payload.name]: parseFloat(
+          (state[payload.name] + payload.value).toFixed(1),
+        ),
+      }
+    case 'handleCarProduction':
+      console.log(payload)
+      return {
+        ...state,
+        [payload.name]: state[payload.name] + payload.value,
+        doors: parseFloat((state.doors - payload.doors).toFixed(1)),
+        engines: parseFloat((state.engines - payload.engines).toFixed(1)),
+        chassis: parseFloat((state.chassis - payload.chassis).toFixed(1)),
+        tires: parseFloat((state.tires - payload.tires).toFixed(1)),
+      }
     default:
       throw new Error()
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,151 @@
-import { Button, Grid, TextField } from '@mui/material'
-import { useContext } from 'react'
-import { Context } from '../App'
+import { Badge, Button, Grid, TextField, Typography } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
 import Tooltip from '@mui/material/Tooltip'
+
+import { Context } from '../App'
+import doorIcon from '../assets/img/car-door.png'
+import chassisIcon from '../assets/img/chassis.png'
+import tiresIcon from '../assets/img/tires.png'
+import engineIcon from '../assets/img/engine.png'
 
 export default function Home() {
   const [state, dispatch] = useContext(Context)
-  const { carsA, carsB } = state
+  const [carAcapacity, setCarAcapacity] = useState(0)
+  const [carBcapacity, setCarBcapacity] = useState(0)
+
+  const { carsA, carsB, staff, tires, doors, chassis, engines } = state
   const onChange = (e) => {
     dispatch({ type: 'handleStaff', payload: e.target.value })
   }
+
+  useEffect(() => {
+    getCarProductionCapacity('carsA')
+    getCarProductionCapacity('carsB')
+  })
+
+  const CAPACITY_PER_EMPLOYEE = {
+    carsA: 0.5,
+    carsB: 0.6,
+    chassis: 2,
+    doors: 1,
+    tires: 0.2,
+    engines: 3,
+  }
+
+  const handleProduction = (product) => {
+    dispatch({
+      type: 'handleProduction',
+      payload: {
+        name: product,
+        value: getTotalCapacity(product),
+      },
+    })
+  }
+
+  const handleCarProduction = (product) => {
+    const deductableTires =
+      product === 'carsA' ? 4 * carAcapacity : 6 * carBcapacity
+    const deductableEngines =
+      product === 'carsA' ? 1 * carAcapacity : 1 * carBcapacity
+    const deductableDoors =
+      product === 'carsA' ? 2 * carAcapacity : 4 * carBcapacity
+    const deductableChassis =
+      product === 'carsA' ? 1 * carAcapacity : 1 * carBcapacity
+    dispatch({
+      type: 'handleCarProduction',
+      payload: {
+        name: product,
+        value: product === 'carsA' ? carAcapacity : carBcapacity,
+        tires: deductableTires,
+        chassis: deductableChassis,
+        doors: deductableDoors,
+        engines: deductableEngines,
+      },
+    })
+  }
+
+  const getTotalCapacity = (article) => {
+    const totalCapacity = (1 / CAPACITY_PER_EMPLOYEE[article]) * staff
+    return parseFloat(totalCapacity.toFixed(1))
+  }
+
+  const getCarProductionCapacity = (model) => {
+    const constraint = Math.min(engines, chassis)
+    const requireTires = model === 'carsA' ? 4 : 6
+    const requireDoors = model === 'carsA' ? 2 : 4
+
+    let totalCapacity = getTotalCapacity(model)
+
+    if (constraint < totalCapacity) totalCapacity = constraint
+    if (
+      tires < requireTires ||
+      chassis < 1 ||
+      doors < requireDoors ||
+      engines < 1
+    ) {
+      return model === 'carsA' ? setCarAcapacity(0) : setCarBcapacity(0)
+    }
+    for (let i = 0; i === totalCapacity; i++) {
+      if (tires >= 4 * totalCapacity && doors >= 2 * totalCapacity) {
+        totalCapacity = i
+      }
+    }
+    // return totalCapacity
+
+    model === 'carsA'
+      ? setCarAcapacity(Math.round(Math.floor(totalCapacity)))
+      : setCarBcapacity(Math.round(Math.floor(totalCapacity)))
+  }
+
   return (
     <>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        style={{ marginTop: 30 }}
+        spacing={{ xs: 1, sm: 3, md: 4 }}
+      >
+        <Grid item>
+          <Badge badgeContent={tires} color="primary">
+            <img src={tiresIcon} alt="tire icon" style={{ width: 42 }} />
+          </Badge>
+        </Grid>
+        <Grid item>
+          <Badge badgeContent={doors} color="primary">
+            <img src={doorIcon} alt="door icon" style={{ width: 42 }} />
+          </Badge>
+        </Grid>
+        <Grid item>
+          <Badge badgeContent={chassis} color="primary">
+            <img src={chassisIcon} alt="chassis icon" style={{ width: 42 }} />
+          </Badge>
+        </Grid>
+        <Grid item>
+          <Badge badgeContent={engines} color="primary">
+            <img src={engineIcon} alt="engine icon" style={{ width: 42 }} />
+          </Badge>
+        </Grid>
+      </Grid>
       <Grid
         container
         spacing={2}
         direction="row"
         alignItems="center"
-        style={{ marginTop: 100 }}
+        style={{ marginTop: 40 }}
       >
         <Grid item xs={6} style={{ textAlign: 'center' }}>
           <div>A model</div>
-          {carsA}
+          <Typography variant="h2" component="h2">
+            {carsA}
+          </Typography>
         </Grid>
         <Grid item xs={6} style={{ textAlign: 'center' }}>
           <div>B model</div>
-          {carsB}
+          <Typography variant="h2" component="h2">
+            {carsB}
+          </Typography>
         </Grid>
       </Grid>
 
@@ -37,23 +158,47 @@ export default function Home() {
         spacing={{ xs: 1, sm: 3, md: 4 }}
       >
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">Tires</Button>
+          <Tooltip title={`${getTotalCapacity('tires')} / day`}>
+            <Button
+              onClick={() => handleProduction('tires')}
+              variant="contained"
+              disabled={staff === 0}
+            >
+              Tires production
+            </Button>
           </Tooltip>
         </Grid>
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">Engines</Button>
+          <Tooltip title={`${getTotalCapacity('engines')} / day`}>
+            <Button
+              onClick={() => handleProduction('engines')}
+              variant="contained"
+              disabled={staff === 0}
+            >
+              Engines production
+            </Button>
           </Tooltip>
         </Grid>
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">Doors</Button>
+          <Tooltip title={`${getTotalCapacity('doors')} / day`}>
+            <Button
+              onClick={() => handleProduction('doors')}
+              variant="contained"
+              disabled={staff === 0}
+            >
+              Doors production
+            </Button>
           </Tooltip>
         </Grid>
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">Chassis</Button>
+          <Tooltip title={`${getTotalCapacity('chassis')} / day`}>
+            <Button
+              onClick={() => handleProduction('chassis')}
+              variant="contained"
+              disabled={staff === 0}
+            >
+              Chassis production
+            </Button>
           </Tooltip>
         </Grid>
       </Grid>
@@ -66,13 +211,25 @@ export default function Home() {
         spacing={{ xs: 1, sm: 3, md: 4 }}
       >
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">CarsA</Button>
+          <Tooltip title={carAcapacity}>
+            <Button
+              variant="contained"
+              disabled={carAcapacity === 0}
+              onClick={() => handleCarProduction('carsA')}
+            >
+              Cars A
+            </Button>
           </Tooltip>
         </Grid>
         <Grid item>
-          <Tooltip title="Delete">
-            <Button variant="contained">CarsB</Button>
+          <Tooltip title={carBcapacity}>
+            <Button
+              variant="contained"
+              disabled={carBcapacity === 0}
+              onClick={() => handleCarProduction('carsB')}
+            >
+              Cars B
+            </Button>
           </Tooltip>
         </Grid>
       </Grid>
