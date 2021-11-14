895be31200c8a581fa6c8e2fa0056f538498a85b
samReact <sam.react001@gmail.com>
2021-11-14 09:53:37 +0100

    :wrench: update i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -24,9 +24,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    react: {},
     fallbackLng: 'en',
-    debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
