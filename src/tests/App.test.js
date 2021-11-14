51a04c22074df7862d179470c48dc7692dc32b92
samReact <sam.react001@gmail.com>
2021-11-14 09:31:27 +0100

    :white_check_mark: add test setup

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import App from '../App'
+import { render, screen } from './test-utils'
+import App from '../pages/App'
 
-test('renders learn react link', () => {
+test('renders two navigation link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const linkElement = screen.getAllByRole('link')
+  expect(linkElement).toHaveLength(2)
 })
