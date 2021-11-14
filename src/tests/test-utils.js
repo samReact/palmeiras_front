d12a75634264969f4dd53ea826d77eaf704f23e0
samReact <sam.react001@gmail.com>
2021-11-14 11:59:26 +0100

    :recycle: test set up

--- src/tests/test-utils.js
+++ src/tests/test-utils.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import '../i18n'
+import { Context, initialState, reducer } from '../store/appReducer'
 
 const AllTheProviders = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <Context.Provider value={[state, dispatch]}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </Context.Provider>
+  )
 }
 
 const customRender = (ui, options) =>
